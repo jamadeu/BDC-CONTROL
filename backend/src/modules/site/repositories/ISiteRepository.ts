6bd4584c74938e1ff36cf08559a55f8bed500edc
Jean Amadeu <jean.amadeu2@gmail.com>
2020-06-20 17:38:06 -0300

    Fix create function arg name

--- backend/src/modules/site/repositories/ISiteRepository.ts
+++ backend/src/modules/site/repositories/ISiteRepository.ts
@@ -7,7 +7,7 @@ import Site from '@modules/site/infra/typeorm/entities/Site';
 import ICreateSiteDTO from '@modules/site/dtos/ICreateSiteDTO';
 
 export default interface ISiteRepository {
-  create(date: ICreateSiteDTO): Promise<Site>;
+  create(data: ICreateSiteDTO): Promise<Site>;
   update(site: Site): Promise<Site>;
   findById(id: number): Promise<Site | undefined>;
   findByNameSite(name: string): Promise<Site | undefined>;

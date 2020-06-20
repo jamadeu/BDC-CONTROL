0d78818eaaf0b5be435a2aeba34bea40afa76577
Jean Amadeu <jean.amadeu2@gmail.com>
2020-06-20 17:07:02 -0300

    Create entity Asset

---
+++ backend/src/modules/asset/infra/typeorm/entities/Asset.ts
@@ -0,0 +1,48 @@
+/*
+ * @Author: Jean Amadeu
+ * @Last Modified by:   Jean Amadeu
+ */
+
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import Site from '@modules/site/infra/typeorm/entities/Site';
+
+@Entity('tb_asset')
+class Asset {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  partnumber: string;
+
+  @Column()
+  serie: string;
+
+  @Column({ unique: true })
+  partnumber_serie: string;
+
+  @Column()
+  status: string;
+
+  @Column()
+  site_id: number;
+
+  @ManyToOne(() => Site, { eager: true })
+  @JoinColumn({ name: 'site_id' })
+  site: Site;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Asset;

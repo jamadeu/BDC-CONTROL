a0c28894c39a0e4c6ec08a3bed8f716dd8867ccb
Jean Amadeu <jean.amadeu2@gmail.com>
2020-06-20 13:46:02 -0300

    Create service to list all sites and service tests

---
+++ backend/src/modules/site/services/ListAllSitesService.spec.ts
@@ -0,0 +1,29 @@
+import FakeSiteRepository from '@modules/site/repositories/fakes/FakeSiteRepository';
+import CreateSiteService from './CreateSiteService';
+import ListAllSites from './ListAllSitesService';
+
+let fakeSiteRepository: FakeSiteRepository;
+let createSite: CreateSiteService;
+let listSites: ListAllSites;
+
+describe('UpdateSite', () => {
+  beforeEach(() => {
+    fakeSiteRepository = new FakeSiteRepository();
+    createSite = new CreateSiteService(fakeSiteRepository);
+    listSites = new ListAllSites(fakeSiteRepository);
+  });
+
+  it('be able to list all sites', async () => {
+    const siteOne = await createSite.execute({
+      name: 'site1',
+    });
+
+    const siteTwo = await createSite.execute({
+      name: 'site2',
+    });
+
+    const listSite = await listSites.execute();
+
+    expect(listSite).toEqual(expect.arrayContaining([siteOne, siteTwo]));
+  });
+});
---
+++ backend/src/modules/site/services/ListAllSitesService.ts
@@ -0,0 +1,17 @@
+import { inject, injectable } from 'tsyringe';
+import Site from '@modules/site/infra/typeorm/entities/Site';
+import ISiteRepository from '../repositories/ISiteRepository';
+
+@injectable()
+class ListAllSitesService {
+  constructor(
+    @inject('SiteRepository')
+    private siteRepository: ISiteRepository
+  ) {}
+
+  public async execute(): Promise<Site[]> {
+    return this.siteRepository.findAll();
+  }
+}
+
+export default ListAllSitesService;

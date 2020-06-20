88e08131b41a3dfcfae7ebf934a15d72fd23d0e8
Jean Amadeu <jean.amadeu2@gmail.com>
2020-06-20 14:16:26 -0300

    Create SiteController

---
+++ backend/src/modules/site/infra/http/controllers/SiteController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import CreateSiteService from '@modules/site/services/CreateSiteService';
+import UpdateSiteService from '@modules/site/services/UpdateSiteService';
+import ListAllSitesService from '@modules/site/services/ListAllSitesService';
+
+export default class SiteController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { name } = request.body;
+    const createSite = container.resolve(CreateSiteService);
+    const site = await createSite.execute(name);
+    return response.json(site);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { id, name } = request.body;
+    const updateSite = container.resolve(UpdateSiteService);
+    const site = await updateSite.execute({ id, name });
+    return response.json(site);
+  }
+
+  public async index(request: Request, response: Response): Promise<Response> {
+    const listSites = container.resolve(ListAllSitesService);
+    const sites = await listSites.execute();
+    return response.json(sites);
+  }
+}

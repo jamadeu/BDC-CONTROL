71002751bb6209fa600ae84e93bdf0650b5a2fd3
Jean Amadeu <jean.amadeu2@gmail.com>
2020-06-20 17:22:12 -0300

    create interface dto to create asset

---
+++ backend/src/modules/asset/dtos/ICreateAssetDTO.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: Jean Amadeu
+ * @Last Modified by:   Jean Amadeu
+ */
+
+export default interface ICreateAssetDTO {
+  partnumber: string;
+  serie: string;
+  status: string;
+  site_id: number;
+}

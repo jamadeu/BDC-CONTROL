f984c02f4f018aea36d3c2b3377acd14eb9a72ad
Jean Amadeu <jean.amadeu2@gmail.com>
2020-06-20 10:38:41 -0300

    Create migraton CreateTableSite

---
+++ backend/src/shared/infra/typeorm/migrations/1592660224054-CreateTableSite.ts
@@ -0,0 +1,38 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateTableSite1592660224054
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'tb_site',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'site',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('tb_site');
+  }
+}

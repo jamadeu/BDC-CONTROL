e0f8d8ce971442430162a929e1fbceafeb6ce0c3
Jean Amadeu <jean.amadeu2@gmail.com>
2020-06-20 10:13:41 -0300

    Create server

---
+++ backend/src/shared/infra/http/server.ts
@@ -0,0 +1,28 @@
+import 'reflect-metadata';
+import 'express-async-errors';
+import '@shared/infra/typeorm';
+import express, { Request, Response, NextFunction } from 'express';
+import AppError from '@shared/errors/AppError';
+import routes from './routes';
+
+const app = express();
+app.use(express.json());
+app.use(routes);
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+app.listen(3333);
+console.log('Server up');

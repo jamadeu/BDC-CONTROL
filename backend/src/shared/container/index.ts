4f0de7acbcb018e8028a48a87ade0c5d6e215ec5
Jean Amadeu <jean.amadeu2@gmail.com>
2020-06-21 06:45:33 -0300

    Config dependency injection to asset repository

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -8,4 +8,12 @@ import { container } from 'tsyringe';
 import ISiteRepository from '@modules/site/repositories/ISiteRepository';
 import SiteRepository from '@modules/site/infra/typeorm/repositories/SiteRepository';
 
+import IAssetRepository from '@modules/asset/repositories/IAssetRepository';
+import AssetRepository from '@modules/asset/infra/typeorm/repositories/AssetRepository';
+
 container.registerSingleton<ISiteRepository>('SiteRepository', SiteRepository);
+
+container.registerSingleton<IAssetRepository>(
+  'AssetRepository',
+  AssetRepository
+);

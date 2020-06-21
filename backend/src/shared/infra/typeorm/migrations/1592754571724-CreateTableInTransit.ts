57caef8e5cfa0a8f6f432dc12ff28eaa4a25d7e0
Jean Amadeu <jean.amadeu2@gmail.com>
2020-06-21 13:13:09 -0300

    Create migration to tb_in_transit

---
+++ backend/src/shared/infra/typeorm/migrations/1592754571724-CreateTableInTransit.ts
@@ -0,0 +1,114 @@
+/*
+ * @Author: Jean Amadeu
+ */
+
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreateTableInTransit1592754571724 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'tb_in_transit',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'asset_id',
+            type: 'int',
+          },
+          {
+            name: 'site_origem_id',
+            type: 'int',
+          },
+          {
+            name: 'site_destination_id',
+            type: 'int',
+          },
+          {
+            name: 'invoice',
+            type: 'varchar',
+          },
+          {
+            name: 'prev',
+            type: 'varchar',
+          },
+          {
+            name: 'status',
+            type: 'varchar',
+          },
+          {
+            name: 'note',
+            type: 'varchar',
+          },
+          {
+            name: 'delivered',
+            type: 'boolean',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      'tb_in_transit',
+      new TableForeignKey({
+        name: 'SiteOrigem',
+        columnNames: ['site_origem_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'tb_site',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      'tb_in_transit',
+      new TableForeignKey({
+        name: 'SiteDestination',
+        columnNames: ['site_destination_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'tb_site',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      'tb_in_transit',
+      new TableForeignKey({
+        name: 'AssetInTransit',
+        columnNames: ['asset_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'tb_asset',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('tb_in_transit', 'AssetInTransit');
+    await queryRunner.dropForeignKey('tb_in_transit', 'SiteDestination');
+    await queryRunner.dropForeignKey('tb_in_transit', 'SiteOrigem');
+    await queryRunner.dropTable('tb_in_transit');
+  }
+}

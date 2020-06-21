74f6f181a35e99c31f76a049b89deb1a728e4f05
Jean Amadeu <jean.amadeu2@gmail.com>
2020-06-21 08:48:38 -0300

    Create service to find an asset  and its testing

---
+++ backend/src/modules/asset/services/FindAssetByScanService.spec.ts
@@ -0,0 +1,50 @@
+/*
+ * @Author: Jean Amadeu
+ */
+
+import AppError from '@shared/errors/AppError';
+import FakeSiteRepository from '@modules/site/repositories/fakes/FakeSiteRepository';
+import CreateSiteService from '@modules/site/services/CreateSiteService';
+import FindByScanService from './FindAssetByScanService';
+import FakeAssetRepository from '../repositories/fakes/FakeAssetRepository';
+
+import CreateAssetRepositoy from './CreateAssetService';
+
+let fakeAssetRepository: FakeAssetRepository;
+let findByScan: FindByScanService;
+let createAsset: CreateAssetRepositoy;
+let fakeSiteRepository: FakeSiteRepository;
+let createSite: CreateSiteService;
+
+describe('FindAssetByScan', () => {
+  beforeEach(() => {
+    fakeAssetRepository = new FakeAssetRepository();
+    findByScan = new FindByScanService(fakeAssetRepository);
+    fakeSiteRepository = new FakeSiteRepository();
+    createSite = new CreateSiteService(fakeSiteRepository);
+    createAsset = new CreateAssetRepositoy(
+      fakeAssetRepository,
+      fakeSiteRepository
+    );
+  });
+
+  it('be able to find an asset by scanning', async () => {
+    const site = await createSite.execute({
+      name: 'site',
+    });
+    const asset = await createAsset.execute({
+      partnumber: 'partnumber',
+      serie: 'serie',
+      site_id: site.id,
+    });
+    const scan = `1S${asset.partnumber}${asset.serie}`.toUpperCase();
+    const foundAsset = await findByScan.execute({ scan });
+
+    expect(foundAsset).toEqual(asset);
+  });
+
+  it('not be able to find an asset that does not exist', async () => {
+    const scan = 'non-exists-scan';
+    await expect(findByScan.execute({ scan })).rejects.toBeInstanceOf(AppError);
+  });
+});
---
+++ backend/src/modules/asset/services/FindAssetByScanService.ts
@@ -0,0 +1,32 @@
+/*
+ * @Author: Jean Amadeu
+ */
+import { inject, injectable } from 'tsyringe';
+import Asset from '@modules/asset/infra/typeorm/entities/Asset';
+import AppError from '@shared/errors/AppError';
+import IAssetRepository from '../repositories/IAssetRepository';
+
+interface IRequest {
+  scan: string;
+}
+
+@injectable()
+class FindAssetByScanService {
+  constructor(
+    @inject('AssetRepository')
+    private assetRepository: IAssetRepository
+  ) {}
+
+  public async execute({ scan }: IRequest): Promise<Asset> {
+    const foundAsset = await this.assetRepository.findByScan(
+      scan.toUpperCase()
+    );
+
+    if (!foundAsset) {
+      throw new AppError('Asset not found');
+    }
+    return foundAsset;
+  }
+}
+
+export default FindAssetByScanService;

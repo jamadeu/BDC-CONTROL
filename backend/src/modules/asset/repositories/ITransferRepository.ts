05ba337ddd381924bf3450ac18ebcecd9a0c18be
Jean Amadeu <jean.amadeu2@gmail.com>
2020-07-05 10:58:37 -0300

    Add function findById

--- backend/src/modules/asset/repositories/ITransferRepository.ts
+++ backend/src/modules/asset/repositories/ITransferRepository.ts
@@ -4,4 +4,5 @@ import ITransferOutDTO from '@modules/asset/dtos/ITransferOutDTO';
 export default interface ITransferRepository {
   transferOut(data: ITransferOutDTO): Promise<InTransit>;
   toReceive(inTransit: InTransit): Promise<InTransit>;
+  findById(in_transit_id: number): Promise<InTransit | undefined>;
 }

0e7f9c82cdf206b4896db1e9f486d6c62222e30f
Jean Amadeu <jean.amadeu2@gmail.com>
2020-07-05 11:01:02 -0300

    First test to receive an inTransit pass

--- backend/src/modules/asset/services/ReceiveAssetService.spec.ts
+++ backend/src/modules/asset/services/ReceiveAssetService.spec.ts
@@ -1,1 +1,64 @@
 import FakeTransferRepsitory from '@modules/asset/repositories/fakes/FakeTransferRepository';
+import FakeAssetRepository from '@modules/asset/repositories/fakes/FakeAssetRepository';
+import FakeSiteRepository from '@modules/site/repositories/fakes/FakeSiteRepository';
+import CreateSiteService from '@modules/site/services/CreateSiteService';
+import CreateAssetRepositoy from './CreateAssetService';
+import TransferOutService from './TransferOutService';
+import ReceiveAssetService from './ReceiveAssetService';
+
+let fakeTransferRepsitory: FakeTransferRepsitory;
+let transfer: TransferOutService;
+let fakeAssetRepository: FakeAssetRepository;
+let createAsset: CreateAssetRepositoy;
+let fakeSiteRepository: FakeSiteRepository;
+let createSite: CreateSiteService;
+let receive: ReceiveAssetService;
+
+describe('ReceiveAsset', () => {
+  beforeEach(() => {
+    fakeAssetRepository = new FakeAssetRepository();
+    fakeSiteRepository = new FakeSiteRepository();
+    createSite = new CreateSiteService(fakeSiteRepository);
+    createAsset = new CreateAssetRepositoy(
+      fakeAssetRepository,
+      fakeSiteRepository
+    );
+    fakeTransferRepsitory = new FakeTransferRepsitory();
+    transfer = new TransferOutService(
+      fakeTransferRepsitory,
+      fakeAssetRepository,
+      fakeSiteRepository
+    );
+    receive = new ReceiveAssetService(
+      fakeTransferRepsitory,
+      fakeAssetRepository
+    );
+  });
+
+  it('be able to receive an asset that is in transit', async () => {
+    const site1 = await createSite.execute({
+      name: 'site1',
+    });
+    const site2 = await createSite.execute({
+      name: 'site2',
+    });
+    const asset = await createAsset.execute({
+      partnumber: 'partnumber',
+      serie: 'serie',
+      site_id: site1.id,
+    });
+
+    const inTransit = await transfer.execute({
+      asset_id: asset.id,
+      site_destination_id: site2.id,
+      invoice: 'invoice',
+    });
+
+    const in_transit_id = inTransit.id;
+    const receivedInTransit = await receive.execute({ in_transit_id });
+    const receivedAsset = await fakeAssetRepository.findById(asset.id);
+
+    expect(receivedInTransit.delivered).toBeTruthy();
+    expect(receivedAsset.site_id).toBe(site2.id);
+  });
+});
---
+++ backend/src/modules/asset/services/ReceiveAssetService.ts
@@ -0,0 +1,36 @@
+import { inject, injectable } from 'tsyringe';
+import InTransit from '@modules/asset/infra/typeorm/entities/InTransit';
+import ITransferRepository from '@modules/asset/repositories/ITransferRepository';
+import IAssetRepository from '@modules/asset/repositories/IAssetRepository';
+import AppError from '@shared/errors/AppError';
+
+interface IRequest {
+  in_transit_id: number;
+}
+
+@injectable()
+class ReceiveAssetService {
+  constructor(
+    @inject('TransferRepository')
+    private transferRepository: ITransferRepository,
+
+    @inject('AssetRepository')
+    private assetRepository: IAssetRepository
+  ) {}
+
+  public async execute({ in_transit_id }: IRequest): Promise<InTransit> {
+    const inTransit = await this.transferRepository.findById(in_transit_id);
+    if (!inTransit) {
+      throw new AppError('Intransit not found');
+    }
+
+    await this.assetRepository.changesAssetLocation(
+      inTransit.asset_id,
+      inTransit.site_destination_id
+    );
+
+    return this.transferRepository.toReceive(inTransit);
+  }
+}
+
+export default ReceiveAssetService;

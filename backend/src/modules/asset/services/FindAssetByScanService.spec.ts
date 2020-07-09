4a3f551782d141e0f3e787ab8a60e8b017418183
Jean Amadeu <jean.amadeu2@gmail.com>
2020-07-09 13:47:32 -0300

    Chore: mock some services

--- backend/src/modules/asset/services/FindAssetByScanService.spec.ts
+++ backend/src/modules/asset/services/FindAssetByScanService.spec.ts
@@ -1,35 +1,27 @@
 import AppError from '@shared/errors/AppError';
 import FakeSiteRepository from '@modules/site/repositories/fakes/FakeSiteRepository';
-import CreateSiteService from '@modules/site/services/CreateSiteService';
 import FindByScanService from './FindAssetByScanService';
 import FakeAssetRepository from '../repositories/fakes/FakeAssetRepository';
-import CreateAssetRepositoy from './CreateAssetService';
 
 let fakeAssetRepository: FakeAssetRepository;
 let findByScan: FindByScanService;
-let createAsset: CreateAssetRepositoy;
 let fakeSiteRepository: FakeSiteRepository;
-let createSite: CreateSiteService;
 
 describe('FindAssetByScan', () => {
   beforeEach(() => {
     fakeAssetRepository = new FakeAssetRepository();
-    findByScan = new FindByScanService(fakeAssetRepository);
     fakeSiteRepository = new FakeSiteRepository();
-    createSite = new CreateSiteService(fakeSiteRepository);
-    createAsset = new CreateAssetRepositoy(
-      fakeAssetRepository,
-      fakeSiteRepository
-    );
+    findByScan = new FindByScanService(fakeAssetRepository);
   });
 
   it('be able to find an asset by scanning', async () => {
-    const site = await createSite.execute({
+    const site = await fakeSiteRepository.create({
       name: 'site',
     });
-    const asset = await createAsset.execute({
+    const asset = await fakeAssetRepository.create({
       partnumber: 'partnumber',
       serie: 'serie',
+      partnumber_serie: '1SPARTNUMBERSERIE',
       site_id: site.id,
     });
     const scan = `1S${asset.partnumber}${asset.serie}`.toUpperCase();

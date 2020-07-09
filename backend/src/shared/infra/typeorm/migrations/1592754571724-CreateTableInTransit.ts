f7d054bb777c97725b6e821086a28bda37c7b9c8
Jean Amadeu <jean.amadeu2@gmail.com>
2020-07-09 13:08:56 -0300

    Chore: Change id type to uuid on table in_transit

--- backend/src/shared/infra/typeorm/migrations/1592754571724-CreateTableInTransit.ts
+++ backend/src/shared/infra/typeorm/migrations/1592754571724-CreateTableInTransit.ts
@@ -7,28 +7,31 @@ import {
 
 export class CreateTableInTransit1592754571724 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+
     await queryRunner.createTable(
       new Table({
         name: 'in_transit',
         columns: [
           {
             name: 'id',
-            type: 'int',
+            type: 'uuid',
             isPrimary: true,
             isGenerated: true,
-            generationStrategy: 'increment',
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'asset_id',
-            type: 'int',
+            type: 'varchar',
           },
           {
             name: 'site_origem_id',
-            type: 'int',
+            type: 'varchar',
           },
           {
             name: 'site_destination_id',
-            type: 'int',
+            type: 'varchar',
           },
           {
             name: 'invoice',

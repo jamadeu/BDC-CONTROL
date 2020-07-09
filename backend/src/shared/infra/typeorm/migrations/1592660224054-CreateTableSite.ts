88c5fc94d54266bb39c56a719e4508ad39b40452
Jean Amadeu <jean.amadeu2@gmail.com>
2020-07-09 09:48:50 -0300

    Chore: Change tid column type
    change id column type from integer to uuid

--- backend/src/shared/infra/typeorm/migrations/1592660224054-CreateTableSite.ts
+++ backend/src/shared/infra/typeorm/migrations/1592660224054-CreateTableSite.ts
@@ -3,16 +3,19 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 export default class CreateTableSite1592660224054
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+
     await queryRunner.createTable(
       new Table({
         name: 'sites',
         columns: [
           {
             name: 'id',
-            type: 'int',
+            type: 'uuid',
             isPrimary: true,
             isGenerated: true,
-            generationStrategy: 'increment',
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'name',

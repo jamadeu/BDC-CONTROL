d8dcd916d215c07132bcbf2b5c66547dd1ab3a49
Jean Amadeu <jean.amadeu2@gmail.com>
2020-07-09 10:34:26 -0300

    Chore: Changes to use id type uuid

--- backend/src/shared/infra/typeorm/migrations/1592682226847-CreateTableAsset.ts
+++ backend/src/shared/infra/typeorm/migrations/1592682226847-CreateTableAsset.ts
@@ -7,16 +7,19 @@ import {
 
 export class CreateTableAsset1592682226847 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+
     await queryRunner.createTable(
       new Table({
         name: 'assets',
         columns: [
           {
             name: 'id',
-            type: 'int',
+            type: 'uuid',
             isPrimary: true,
             isGenerated: true,
-            generationStrategy: 'increment',
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'partnumber',
